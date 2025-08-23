5d2b67c352dfe98e902aee1b88e9a6c33ba9b2b9
ap-gautham <apgautham@gmail.com>
2025-08-23 01:18:25 -0400

    ‘Message’

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
     site: 'https://ap-gautham.github.io',
-    base: '/ap-gautham',
+    base: '/',
   integrations: [mdx(), sitemap(), tailwind()]
 });
 
