b53543944187441d0a59630a93d269a6e6a257e2
ap-gautham <apgautham@gmail.com>
2025-08-23 00:10:18 -0400

    ‘Message’

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
-
+// @ts-check
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://ap-gautham.github.io',
-  base: '/ap-gautham',
-  integrations: [mdx(), sitemap(), tailwind()]
+    site: 'https://ap-gautham.github.io',
+    base: '/github-pages'
 });
